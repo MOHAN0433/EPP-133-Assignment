66b534b11c670e37df204d9410ecab7dbcd37913
Mohan Patil <mohan.patil@hyniva.com>
2024-02-22 11:45:16 +0530

    filter condition new n

--- src/Lamda/metadata/metadataHandler.js
+++ src/Lamda/metadata/metadataHandler.js
@@ -1,76 +1,65 @@
 const {
-    DynamoDBClient,
-    PutItemCommand,
-    UpdateItemCommand,
-    DeleteItemCommand,
-    GetItemCommand,
-    ScanCommand,
-    QueryCommand,
-  } = require("@aws-sdk/client-dynamodb");
-  const { marshall, unmarshall } = require("@aws-sdk/util-dynamodb");
-  const moment = require("moment");
-  const client = new DynamoDBClient();
-  const {
-    httpStatusCodes,
-    httpStatusMessages,
-  } = require("../../environment/appconfig");
-  const currentDate = Date.now(); // get the current date and time in milliseconds
-  const formattedDate = moment(currentDate).format("YYYY-MM-DD HH:mm:ss"); // formatting date
-  
-  const filterItems = (items, filterConditions) => {
-    // If no filter conditions provided, return items as it is
-    if (!filterConditions || Object.keys(filterConditions).length === 0) {
-        return items;
-    }
+  DynamoDBClient,
+  PutItemCommand,
+  UpdateItemCommand,
+  DeleteItemCommand,
+  GetItemCommand,
+  ScanCommand,
+  QueryCommand,
+} = require("@aws-sdk/client-dynamodb");
+const { marshall, unmarshall } = require("@aws-sdk/util-dynamodb");
+const moment = require("moment");
+const client = new DynamoDBClient();
+const {
+  httpStatusCodes,
+  httpStatusMessages,
+} = require("../../environment/appconfig");
+const currentDate = Date.now(); // get the current date and time in milliseconds
+const formattedDate = moment(currentDate).format("YYYY-MM-DD HH:mm:ss"); // formatting date
 
-    // Filter items based on filter conditions
-    return items.filter(item => {
-        for (let key in filterConditions) {
-            if (item[key] && item[key].S !== filterConditions[key]) {
-                return false;
-            }
-        }
-        return true;
-    });
-};
-
-const getAllMeatadatas = async (event) => {
-  const filterConditions = event.queryStringParameters || {}; // Extract filter conditions from query parameters
+const getAllMeatadatas = async () => {
   const response = { statusCode: httpStatusCodes.SUCCESS };
   try {
-      const { Items } = await client.send(
-          new ScanCommand({ TableName: process.env.METADATA_TABLE })
+    const { Items } = await client.send(
+      new ScanCommand({ TableName: process.env.METADATA_TABLE })
+    ); // Getting table name from the servetless.yml and setting to the TableName
+
+    if (Items.length === 0) {
+      // If there is no employee details found
+      response.statusCode = httpStatusCodes.NOT_FOUND; // Setting the status code to 404
+      response.body = JSON.stringify({
+        message: httpStatusMessages.METADATA_DETAILS_NOT_FOUND,
+      }); // Setting error message
+    } else {
+      const sortedItems = Items.sort((a, b) =>
+        a.metadataId.N.localeCompare(b.metadataId.N)
       );
 
-      if (Items.length === 0) {
-          response.statusCode = httpStatusCodes.NOT_FOUND;
-          response.body = JSON.stringify({
-              message: httpStatusMessages.METADATA_DETAILS_NOT_FOUND,
-          });
-      } else {
-          const sortedItems = Items.sort((a, b) =>
-              a.metadataId.N.localeCompare(b.metadataId.N)
-          );
-
-          let filteredItems = sortedItems;
-          if (Object.keys(filterConditions).length > 0) {
-              filteredItems = filterItems(sortedItems, filterConditions);
-          }
-
-          const metadataList = filteredItems.map(item => unmarshall(item));
-
-          response.body = JSON.stringify({
-              message: httpStatusMessages.SUCCESSFULLY_RETRIEVED_METADATA_DETAILS,
-              data: metadataList,
-          });
-      }
-  } catch (e) {
-      console.error(e);
-      response.body = JSON.stringify({
-          statusCode: httpStatusCodes.INTERNAL_SERVER_ERROR,
-          message: httpStatusMessages.FAILED_TO_RETRIEVE_METADATA_DETAILS,
-          errorMsg: e.message,
+      // Map and set "password" field to null
+      const metadataList = sortedItems.map((item) => {
+        const metadata = unmarshall(item);
+      //   if (employee.hasOwnProperty("password")) {
+      //     employee.password = null;
+      //   }
+        return metadata;
       });
+
+      response.body = JSON.stringify({
+        message: httpStatusMessages.SUCCESSFULLY_RETRIEVED_METADATA_DETAILS,
+        data: metadataList,
+      });
+    }
+  } catch (e) {
+    console.error(e);
+    response.body = JSON.stringify({
+      statusCode: httpStatusCodes.INTERNAL_SERVER_ERROR,
+      message: httpStatusMessages.FAILED_TO_RETRIEVE_METADATA_DETAILS,
+      errorMsg: e.message,
+    });
   }
   return response;
+};
+
+module.exports = {
+  getAllMeatadatas,
 };
