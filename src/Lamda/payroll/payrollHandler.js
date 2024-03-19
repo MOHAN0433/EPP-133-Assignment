3895a57aad05511372d49a1280ea69561427adbd
Mohan Patil <mohan.patil@hyniva.com>
2024-03-19 16:50:50 +0530

    pa ra

--- src/Lamda/payroll/payrollHandler.js
+++ src/Lamda/payroll/payrollHandler.js
@@ -58,8 +58,8 @@ const numericFields = [
 ];
 
 for (const field of numericFields) {
-  if (requestBody[field] !== undefined) {
-      if (requestBody[field] === null || requestBody[field] === '' || typeof requestBody[field] !== 'number') {
+  if (requestBody[field] !== undefined || requestBody[field] === null ) {
+      if (requestBody[field] === '' || typeof requestBody[field] !== 'number') {
           throw new Error(`${field} must be a non-null non-empty number if provided.`);
       }
   }
