12db1ab8cd7bf8987373a64d226f05562af7d922
Mohan Patil <mohan.patil@hyniva.com>
2024-04-01 15:21:49 +0530

    branch filter pagemm nmm halppa

--- src/Lamda/employee/employeeHandler.js
+++ src/Lamda/employee/employeeHandler.js
@@ -310,7 +310,7 @@ const getAllEmployees = async (event) => {
       "Access-Control-Allow-Origin": "*",
     },
   };
-  const { pageNo, pageSize, employeeId, name } = event.queryStringParameters;
+  const { pageNo, pageSize } = event.queryStringParameters;
   let designationFilter = [];
   let branchFilter = [];
 
@@ -331,7 +331,7 @@ const getAllEmployees = async (event) => {
 
     // Apply filters
     console.log("Filtering started with designationFilter:", designationFilter, "and branchFilter:", branchFilter);
-    const filteredItems = applyFilters(Items, designationFilter, branchFilter, employeeId, name);
+    const filteredItems = applyFilters(Items, designationFilter, branchFilter);
     console.log("Filtered items:", filteredItems);
 
     // Apply pagination
@@ -379,7 +379,7 @@ const pagination = (allItems, pageNo, pageSize) => {
   };
 };
 
-const applyFilters = (employeesData, designationFilter, branchFilter, employeeId, name) => {
+const applyFilters = (employeesData, designationFilter, branchFilter) => {
   console.log("Applying filters...");
   const filteredEmployees = employeesData.filter(employee => {
     // Check if employee.branch exists before accessing its properties
@@ -388,14 +388,18 @@ const applyFilters = (employeesData, designationFilter, branchFilter, employeeId
     }
     console.log("Employee:", employee);
     const passesDesignationFilter = designationFilter.length === 0 || designationFilter.includes(employee.designation.S);
+    // Note: Use `.S` to access the string value of DynamoDB attributes
     const passesBranchFilter = branchFilter.length === 0 || matchesBranch(employee.branchOffice.S, branchFilter);
-    const passesEmployeeIdSearch = !employeeId || employee.employeeId.S.includes(employeeId);
-    const passesNameSearch = !name || employee.name.S.toLowerCase().includes(name.toLowerCase());
-    
-    const passesFilters = passesDesignationFilter && passesBranchFilter && passesEmployeeIdSearch && passesNameSearch;
+    const passesFilters = passesDesignationFilter && passesBranchFilter;
     return passesFilters;
   });
 
+  // If no filters are specified or if no employees pass the filters, return all employees
+  if (designationFilter.length === 0 && branchFilter.length === 0) {
+    return employeesData;
+  } else if (filteredEmployees.length === 0) {
+    return employeesData;
+  }
   return filteredEmployees;
 };
 
