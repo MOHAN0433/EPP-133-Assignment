7afa6078ab7e9c5eafab2add541e7eeecb473526
Mohan Patil <mohan.patil@hyniva.com>
2024-04-05 15:56:05 +0530

    education changes require

--- src/Lamda/education/educationHandler.js
+++ src/Lamda/education/educationHandler.js
@@ -145,7 +145,7 @@ for (const field of numericFields) {
         Item: marshall({
           educationId: nextSerialNumber,
           degree: requestBody.degree,
-          course: requestBody.cource,
+          course: requestBody.course,
           university: requestBody.university,
           graduationPassingYear : requestBody.graduationPassingYear,
           createdDateTime: formattedDate,
