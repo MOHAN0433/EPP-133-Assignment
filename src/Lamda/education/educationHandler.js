fee17825d0f5deb8eae898a0773a17f87803a820
Mohan Patil <mohan.patil@hyniva.com>
2024-04-16 20:18:27 +0530

    degree changes

--- src/Lamda/education/educationHandler.js
+++ src/Lamda/education/educationHandler.js
@@ -1,52 +1,12 @@
-const AWS = require('aws-sdk');
 const parseMultipart = require('parse-multipart');
 const moment = require("moment");
 const { DynamoDBClient, PutItemCommand } = require("@aws-sdk/client-dynamodb");
 
-const BUCKET = 'education0433123';
-const s3 = new AWS.S3();
 const client = new DynamoDBClient();
-
-function extractFile(event) {
-  const contentType = event.headers['Content-Type'];
-  if (!contentType) {
-    throw new Error('Content-Type header is missing in the request.');
-  }
-
-  const boundary = parseMultipart.getBoundary(contentType);
-  if (!boundary) {
-    throw new Error(
-      'Unable to determine the boundary from the Content-Type header.'
-    );
-  }
-
-  const parts = parseMultipart.Parse(
-    Buffer.from(event.body, 'base64'),
-    boundary
-  );
-
-  if (!parts || parts.length === 0) {
-    throw new Error('No parts found in the multipart request.');
-  }
-
-  const [{ filename, data }] = parts;
-
-  if (!filename || !data) {
-    throw new Error(
-      'Invalid or missing file name or data in the multipart request.'
-    );
-  }
-
-  return {
-    filename,
-    data,
-  };
-}
 
 function extractDegree(event) {
   const contentType = event.headers['Content-Type'];
   console.log('Content-Type:', contentType);
-  //console.log('Event body:', event.body);
 
   if (!contentType) {
     throw new Error('Content-Type header is missing in the request.');
@@ -81,25 +41,13 @@ function extractDegree(event) {
 module.exports.createEducation = async (event) => {
   try {
     console.log('rewuest event:', event.body.degree);
-    const { filename, data } = extractFile(event);
     const degree = extractDegree(event);
 
-    // Upload file to S3
-    await s3.putObject({
-      Bucket: BUCKET,
-      Key: filename,
-      Body: data,
-    }).promise();
-
-    // Construct S3 object URL
-    const s3ObjectUrl = `https://${BUCKET}.s3.amazonaws.com/${filename}`;
-
-    // Save S3 object URL and degree in DynamoDB
+    // Save degree in DynamoDB
     await client.send(new PutItemCommand({
       TableName: process.env.EDUCATION_TABLE,
       Item: {
         educationId: { N: Date.now().toString() }, // Assuming educationId is a number
-        link: { S: s3ObjectUrl },
         degree: { S: degree },
         createdAt: { S: moment().format("YYYY-MM-DD HH:mm:ss") }
       }
@@ -108,7 +56,7 @@ module.exports.createEducation = async (event) => {
     return {
       statusCode: 200,
       body: JSON.stringify({
-        link: s3ObjectUrl,
+        degree: degree,
       }),
     };
   } catch (err) {
