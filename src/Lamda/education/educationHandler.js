4b0367e885bc03af50a838344a4f24aae10721bd
Mohan Patil <mohan.patil@hyniva.com>
2024-04-17 00:03:44 +0530

    degree enatiri new

--- src/Lamda/education/educationHandler.js
+++ src/Lamda/education/educationHandler.js
@@ -10,7 +10,7 @@ exports.createEducation = async (event) => {
     console.log("body", event.body.degree);
     // Extract the degree from the form data
     const degree = event['body'].split('=')[1]; // Extract the value after '='
-    console.log("degree", degree);
+    console.log("degree ", degree);
    
     // Prepare the item to be inserted into DynamoDB
     await client.send(new PutItemCommand({
