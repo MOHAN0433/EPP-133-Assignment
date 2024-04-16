805afe6102c5923b936997c71c62f06792bd59ab
Mohan Patil <mohan.patil@hyniva.com>
2024-04-17 00:16:51 +0530

    degree enatiri new ok mmm

--- src/Lamda/education/educationHandler.js
+++ src/Lamda/education/educationHandler.js
@@ -1,22 +1,26 @@
 const parseMultipart = require('parse-multipart');
 const moment = require("moment");
 const { DynamoDBClient, PutItemCommand } = require("@aws-sdk/client-dynamodb");
- 
+
 const client = new DynamoDBClient();
- 
-const queryString = require('querystring');
 
 exports.createEducation = async (event) => {
   try {
     console.log("body", event.body);
     
-    // Decode the base64 encoded body
-    const decodedBody = Buffer.from(event.body, 'base64').toString('utf-8');
-    console.log("decoded body", decodedBody);
+    // Parse multipart form data
+    const boundary = parseMultipart.getBoundary(event.headers['Content-Type']);
+    const parts = parseMultipart(event.body, boundary);
     
-    // Extract the degree from the decoded form data
-    const parsedBody = queryString.parse(decodedBody);
-    const degree = parsedBody.degree;
+    // Extract degree from parts
+    let degree;
+    parts.forEach(part => {
+      if (part.filename === undefined) {
+        if (part.name === 'degree') {
+          degree = part.data.toString('utf-8');
+        }
+      }
+    });
     console.log("degree", degree);
     
     // Prepare the item to be inserted into DynamoDB
