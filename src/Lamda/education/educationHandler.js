283a379f6e968886b8c22901ffd67fa09b826371
Mohan Patil <mohan.patil@hyniva.com>
2024-04-16 23:13:24 +0530

    degree enatiri

--- src/Lamda/education/educationHandler.js
+++ src/Lamda/education/educationHandler.js
@@ -1,35 +1,35 @@
-const parseMultipart = require('parse-multipart');
-const moment = require("moment");
 const { DynamoDBClient, PutItemCommand } = require("@aws-sdk/client-dynamodb");
+const moment = require("moment");
 
 const client = new DynamoDBClient();
 
 exports.createEducation = async (event) => {
-  try {
-    console.log("body", event.body)
-    // Extract the degree from the form data
-    const degree = event['body'].split('=')[1]; // Extract the value after '='
-    console.log("degree", degree);
-    
-    // Prepare the item to be inserted into DynamoDB
-    await client.send(new PutItemCommand({
-      TableName: process.env.EDUCATION_TABLE,
-      Item: {
-        educationId: { N: Date.now().toString() }, // Assuming educationId is a number
-        degree: { S: degree },
-        createdAt: { S: moment().format("YYYY-MM-DD HH:mm:ss") }
-      }
-    }));
-    
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: 'Degree saved successfully' })
-    };
-} catch (error) {
-    console.error('Error saving degree:', error);
-    return {
-        statusCode: 500,
-        body: JSON.stringify({ message: 'Error saving degree' })
-    };
-}
+    try {
+        // Parse the JSON data from the request body
+        const bodyData = JSON.parse(event.body);
+        
+        // Extract the degree from the parsed JSON data
+        const degree = bodyData.degree;
+        
+        // Prepare the item to be inserted into DynamoDB
+        await client.send(new PutItemCommand({
+            TableName: process.env.EDUCATION_TABLE,
+            Item: {
+                educationId: { N: Date.now().toString() }, // Assuming educationId is a number
+                degree: { S: degree },
+                createdAt: { S: moment().format("YYYY-MM-DD HH:mm:ss") }
+            }
+        }));
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Degree saved successfully' })
+        };
+    } catch (error) {
+        console.error('Error saving degree:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Error saving degree' })
+        };
+    }
 };
