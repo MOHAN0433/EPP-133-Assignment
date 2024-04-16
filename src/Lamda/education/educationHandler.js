2c50f078debb9c676cdcad8088895be26fc661d7
Mohan Patil <mohan.patil@hyniva.com>
2024-04-16 13:16:43 +0530

    upload and save new

--- src/Lamda/education/educationHandler.js
+++ src/Lamda/education/educationHandler.js
@@ -24,6 +24,38 @@ const parseMultipart = require('parse-multipart');
 const BUCKET = 'education0433123'; // Change the bucket name to match the one defined in your serverless 
 const s3 = new AWS.S3();
 
+// Function to extract file from multipart/form-data
+function extractFile(event) {
+  const contentType = event.headers['Content-Type'];
+  if (!contentType) {
+    throw new Error('Content-Type header is missing in the request.');
+  }
+
+  // Boundary
+  const boundary = parseMultipart.getBoundary(contentType);
+  if (!boundary) {
+    throw new Error('Unable to determine the boundary from the Content-Type header.');
+  }
+
+  const parts = parseMultipart.Parse(Buffer.from(event.body, 'base64'), boundary);
+  console.log('--------parts', parts);
+
+  if (!parts || parts.length === 0) {
+    throw new Error('No parts found in the multipart request.');
+  }
+
+  const [{ filename, data }] = parts;
+
+  if (!filename || !data) {
+    throw new Error('Invalid or missing file name or data in the multipart request.');
+  }
+
+  return {
+    filename,
+    data,
+  };
+}
+
 module.exports.createEducation = async (event) => {
   try {
     // Upload file to S3
