c863b7cb08b613cf01c6298ac42926b28e2c8a4f
Mohan Patil <mohan.patil@hyniva.com>
2024-04-17 10:39:23 +0530

    degree enatiri new ok mmm nn

--- src/Lamda/education/educationHandler.js
+++ src/Lamda/education/educationHandler.js
@@ -1,17 +1,30 @@
 const parseMultipart = require('parse-multipart');
 const moment = require("moment");
 const { DynamoDBClient, PutItemCommand } = require("@aws-sdk/client-dynamodb");
- 
+
 const client = new DynamoDBClient();
- 
+
 exports.createEducation = async (event) => {
   try {
     console.log("body", event.body);
-    // Extract the degree from the form data
-    const bodyParts = event['body'].split(':');
-    const degree = bodyParts.length >= 2 ? bodyParts[1].trim() : ''; // Extract the value after ':' and trim any whitespace, or use an empty string if not found
+
+    // Parse multipart form data
+    const boundary = parseMultipart.getBoundary(event.headers['Content-Type']);
+    const parts = parseMultipart(event.body, boundary);
+
+    let degree = '';
+
+    // Iterate over parts to find the degree field
+    parts.forEach(part => {
+      if (part.filename === undefined) {
+        if (part.name === 'degree') {
+          degree = part.data.toString('utf-8');
+        }
+      }
+    });
+
     console.log("degree", degree);
-   
+
     // Prepare the item to be inserted into DynamoDB
     await client.send(new PutItemCommand({
       TableName: process.env.EDUCATION_TABLE,
@@ -21,16 +34,16 @@ exports.createEducation = async (event) => {
         createdAt: { S: moment().format("YYYY-MM-DD HH:mm:ss") }
       }
     }));
-   
+
     return {
-        statusCode: 200,
-        body: JSON.stringify({ message: 'Degree saved successfully' })
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Degree saved successfully' })
     };
   } catch (error) {
     console.error('Error saving degree:', error);
     return {
-        statusCode: 500,
-        body: JSON.stringify({ message: 'Error saving degree' })
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Error saving degree' })
     };
   }
 };
