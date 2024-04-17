e13febc78d65ec000679b629596af32969368062
Mohan Patil <mohan.patil@hyniva.com>
2024-04-17 16:01:29 +0530

    degree enatiri new ok mmm nn okkk new

--- src/Lamda/education/educationHandler.js
+++ src/Lamda/education/educationHandler.js
@@ -18,32 +18,32 @@ const createEducation = async (event) => {
   try {
     const requestBody = JSON.parse(event.body);
     console.log("Request Body:", requestBody);
- 
+
     // Check for required fields
-    // const requiredFields = [
-    //   "degree",
-    //   "course",
-    //   "university",
-    //   "graduationPassingYear",
-    //   "employeeId",
-    // ];
-    // if (!requiredFields.every((field) => requestBody[field])) {
-    //   throw new Error("Required fields are missing.");
-    // }
- 
+    const requiredFields = [
+      "degree",
+      "course",
+      "university",
+      "graduationPassingYear",
+      "employeeId",
+    ];
+    if (!requiredFields.every((field) => requestBody[field])) {
+      throw new Error("Required fields are missing.");
+    }
+
 //       const validatePanNumber = (panNumber) => {
 //         const panRegex = /[A-Z]{5}[0-9]{4}[A-Z]/;
 //         return panRegex.test(panNumber);
 //     };
- 
+
 //     if (!validatePanNumber(requestBody.panNumber)) {
 //       throw new Error("Invalid PAN Number. PAN Number should be in the format ABCDE1234F.");
 //   }
- 
+
 const numericFields = [
 "graduationPassingYear"
 ];
- 
+
 for (const field of numericFields) {
 if (requestBody[field] !== undefined || requestBody[field] !== null ) {
     if (requestBody[field] === '' || typeof requestBody[field] == 'string') {
@@ -51,14 +51,14 @@ if (requestBody[field] !== undefined || requestBody[field] !== null ) {
     }
 }
 }
- 
+
   //   const totalEarnings = requestBody.basicPay + requestBody.HRA + requestBody.medicalAllowances + requestBody.conveyances + requestBody.otherEarnings + requestBody.bonus + requestBody.variablePay + requestBody.enCashment;
   //   const totalDeductions = requestBody.incomeTax + requestBody.professionalTax + requestBody.providentFund;
   //   const totalNetPay = totalEarnings - totalDeductions;
- 
+
     const highestSerialNumber = await getHighestSerialNumber();
     console.log("Highest Serial Number:", highestSerialNumber);
- 
+
     const nextSerialNumber =
       highestSerialNumber !== null ? parseInt(highestSerialNumber) + 1 : 1;
     async function getHighestSerialNumber() {
@@ -68,7 +68,7 @@ if (requestBody[field] !== undefined || requestBody[field] !== null ) {
         Limit: 1,
         ScanIndexForward: false, // Sort in descending order to get the highest serial number first
       };
- 
+
       try {
         const result = await client.send(new ScanCommand(params));
         console.log("DynamoDB Result:", result); // Add this line to see the DynamoDB response
@@ -77,25 +77,25 @@ if (requestBody[field] !== undefined || requestBody[field] !== null ) {
         } else {
           // Parse and return the highest serial number without incrementing
           const educationIdObj = result.Items[0].educationId;
-          console.log("Education ID from DynamoDB:", educationIdObj);
-          const educationId = parseInt(educationIdObj.N);
+          console.log("Education ID from DynamoDB:", educationIdObj); 
+          const educationId = parseInt(educationIdObj.N); 
           console.log("Parsed Education ID:", educationId);
-          return educationId;
+          return payrollId;
         }
       } catch (error) {
         console.error("Error retrieving highest serial number:", error);
         throw error; // Propagate the error up the call stack
       }
     }
- 
+
     // Check if an education already exists for the employee
     const existingEducation = await getEducationByEmployee(
-    requestBody.employeeId
+      requestBody.panNumber, requestBody.employeeId
     );
     if (existingEducation) {
       throw new Error("A Education Details already Employee ID.");
   }
- 
+
     async function getEducationByEmployee(employeeId) {
       const params = {
         TableName: process.env.EDUCATION_TABLE,
@@ -104,16 +104,16 @@ if (requestBody[field] !== undefined || requestBody[field] !== null ) {
           ":employeeId": { S: employeeId },
         },
       };
- 
+
       try {
         const result = await client.send(new ScanCommand(params));
         return result.Items.length > 0;
       } catch (error) {
-        console.error("Error retrieving education by employeeId:", error);
+        console.error("Error retrieving payroll by employeeId:", error);
         throw error;
       }
     }
- 
+
     const checkEmployeeExistence = async (employeeId) => {
       const params = {
         TableName: process.env.EMPLOYEE_TABLE,
@@ -121,7 +121,7 @@ if (requestBody[field] !== undefined || requestBody[field] !== null ) {
           employeeId: employeeId,
         }),
       };
- 
+
       try {
         const result = await client.send(new GetItemCommand(params));
         if (!result.Item) {
@@ -133,20 +133,20 @@ if (requestBody[field] !== undefined || requestBody[field] !== null ) {
       }
     };
     await checkEmployeeExistence(requestBody.employeeId);
- 
+
     const params = {
       TableName: process.env.EDUCATION_TABLE,
       Item: marshall({
         educationId: nextSerialNumber,
         degree: requestBody.degree,
-        course: requestBody.course,
+        cource: requestBody.cource,
         university: requestBody.university,
         graduationPassingYear : requestBody.graduationPassingYear,
         createdDateTime: formattedDate,
         updatedDateTime: null,
       }),
     };
- 
+
     await client.send(new PutItemCommand(params));
     response.body = JSON.stringify({
       message: httpStatusMessages.SUCCESSFULLY_CREATED_EDUCATION_DETAILS,
