5c82b20458198093a96027c63a2eda547016c62d
Mohan Patil <mohan.patil@hyniva.com>
2024-05-03 15:50:05 +0530

    employee check education number

--- src/Lamda/assignment/assignmentHandler.js
+++ src/Lamda/assignment/assignmentHandler.js
@@ -320,23 +320,26 @@ const createAssignment = async (event) => {
     }
 
     const checkEmployeeExistence = async (employeeId) => {
+      // Convert employeeId to number
+      const employeeIdNumber = parseInt(employeeId);
+  
       const params = {
-        TableName: process.env.EMPLOYEE_TABLE,
-        Key: marshall({
-          employeeId: employeeId,
-        }),
+          TableName: process.env.EMPLOYEE_TABLE,
+          Key: marshall({
+              employeeId: employeeIdNumber,
+          }),
       };
-
+  
       try {
-        const result = await client.send(new GetItemCommand(params));
-        if (!result.Item) {
-          throw new Error("Employee not found.");
-        }
+          const result = await client.send(new GetItemCommand(params));
+          if (!result.Item) {
+              throw new Error("Employee not found.");
+          }
       } catch (error) {
-        console.error("Error checking employee existence:", error);
-        throw error;
+          console.error("Error checking employee existence:", error);
+          throw error;
       }
-    };
+  };
     await checkEmployeeExistence(requestBody.employeeId);
 
     const params = {
